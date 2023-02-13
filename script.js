93a5af0b4c3be0525b5c4a559da3486461786ec1
aldrinmalakar <70992493+aldrinmalakar@users.noreply.github.com>
2023-02-12 22:17:01 -0500

    Update Script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+const homeChordSelect = document.getElementById("home-chord");
+const transposeInput = document.getElementById("transpose");
+const transposeButton = document.getElementById("transpose-button");
+const lyricsAndChordsDiv = document.getElementById("lyrics-and-chords");
 
+const chords = [
+  "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"
+];
+
+transposeButton.addEventListener("click", function() {
+  const homeChordIndex = chords.indexOf(homeChordSelect.value);
+  const transpose = transposeInput.value;
+  const newChordIndices = chords.map(function(chord, index) {
+    return (index + transpose + chords.length) % chords.length;
+  });
+
+  // Clear the previous lyrics and chords
+  lyricsAndChordsDiv.innerHTML = "";
+
+  // Add the transposed lyrics and chords for each song here
+});
